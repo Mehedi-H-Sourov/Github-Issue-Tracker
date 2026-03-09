5cacfef1298470eb1617d1145cab396446aa37fe
Mehedi-AgEcon <sourov2454@gmail.com>
2026-03-09 17:17:06 +0100

    first commit

---
+++ script/index.js
@@ -0,0 +1,19 @@
+
+const form = document.getElementById("sign-in-btn");
+
+form.addEventListener("click", function (event) {
+
+    const username = document.getElementById("sign-in-name").value;
+    const password = document.getElementById("sign-in-password").value;
+
+    // validation
+    if (username == "admin" && password == "admin123") {
+
+
+        // redirect to issues page
+        window.location.href ="../issues.html";
+
+    } else {
+        alert("Invalid username or password");
+    }
+});
