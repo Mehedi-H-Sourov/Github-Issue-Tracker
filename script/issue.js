0a96f8ec2b420f92851a4d1013b69beb2b98efb5
Mehedi-AgEcon <sourov2454@gmail.com>
2026-03-11 14:21:30 +0100

    final draft

--- script/issue.js
+++ script/issue.js
@@ -1,4 +1,4 @@
-
+let allIssues = [];
 let openData = [];
 let closeData = [];
 let currentStatus = 'all';
@@ -12,6 +12,8 @@ const loadingSpinner = document.getElementById("loading-spinner");
 const allBtn = document.getElementById("btn-all");
 const openBtn = document.getElementById("btn-open");
 const closeBtn = document.getElementById("btn-close");
+
+const detailsContainer = document.getElementById('modal_details');
 
 // Loading
 function showLoading() {
@@ -29,6 +31,7 @@ async function loadIssues() {
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json();
     hideLoading();
+    allIssues = data.data;
     openData = data.data.filter(open => open.status == 'open')
     closeData = data.data.filter(close => close.status == 'closed')
     displayIssues(data.data);
@@ -38,7 +41,7 @@ async function loadIssues() {
 // showLabels
 4.
 function showLabels(arr) {
-    const spanCreate = arr.map((labels) => `<span class="${labels == 'bug' ? 'bg-red-200 p-2 text-red-500 rounded-xl m-1' : labels == 'enhancement' ? 'bg-orange-200 text-orange-500 rounded-xl p-2 m-1' : labels == 'documentation' ? 'bg-gray-200 text-gray-500 rounded-xl p-2 m-1' : 'bg-yellow-200 text-yellow-500 rounded-xl p-2 m-1'}"> ${labels == 'bug' ? '<i class="fa-solid fa-bug"></i>' : '<i class="fa-solid fa-life-ring"></i>'} ${labels.toUpperCase()}  </span>`)
+    const spanCreate = arr.map((labels) => `<span class="${labels == 'bug' ? 'bg-red-200 p-2 text-red-500 rounded-2xl m-1' : labels == 'enhancement' ? 'bg-orange-200 text-orange-500 rounded-2xl p-2 m-1' : labels == 'documentation' ? 'bg-gray-200 text-gray-500 rounded-2xl p-2 m-1' : 'bg-yellow-200 text-yellow-500 rounded-2xl p-2 m-1'}"> ${labels == 'bug' ? '<i class="fa-solid fa-bug"></i>' : '<i class="fa-solid fa-life-ring"></i>'} ${labels.toUpperCase()}  </span>`)
     return spanCreate.join(' ')
 
 }
@@ -52,19 +55,23 @@ function issueCount() {
 // 2. Displaying all the cards 
 
 const displayIssues = (issues) => {
-    const date = new Date(issues.createdAt);
-    const formattedDate = date.toLocaleDateString();
+
+    allIssuesContainer.innerHTML = "";
 
     for (let issue of issues) {
+        const date = new Date(issue.createdAt);
+        const formattedDate = date.toLocaleDateString();
+
         const cardDiv = document.createElement("div");
 
-        cardDiv.className = `shadow p-4 space-y-3 rounded-md ${issue.status == 'open' ? 'border-t-4 border-green-500' : 'border-t-4 border-purple-500'}`;
+        cardDiv.className = `shadow p-4 space-y-3 rounded-md  ${issue.status == 'open' ? 'border-t-4 border-green-500' : 'border-t-4 border-purple-500'}`;
 
         cardDiv.innerHTML = `
+       <div onclick="modalDetails(${issue.id})"> 
         <div class="flex justify-between ">
         
                 <img src= "${issue.status == 'open' ? '../Open-Status.png' : '../Closed- Status .png'}" alt="open-close" class="h-8 w-8">
-                <p class="px-3 py-2 rounded-full ${issue.priority == 'high'
+                <p  class="px-3 py-2 rounded-full ${issue.priority == 'high'
                 ? 'text-red-500 font-bold bg-red-100'
                 : issue.priority == 'medium'
                     ? 'text-yellow-500 bg-yellow-100 font-bold'
@@ -77,7 +84,7 @@ const displayIssues = (issues) => {
                 <p class="text-gray-500 line-clamp-2">${issue.description}</p>
             </div>
 
-            <div class="flex flex-wrap"> 
+            <div class="flex flex-wrap" > 
             ${showLabels(issue.labels)}
             </div>
             <hr class="border-t-2 border-gray-300 mt-4">
@@ -85,13 +92,15 @@ const displayIssues = (issues) => {
             <p class="text-gray-500 mt-3">${issue.assignee ? issue.assignee : 'anonymous'}</p>
             <br>
             <p class="text-gray-500">${formattedDate}</p>
+       </div>
      `
         allIssuesContainer.appendChild(cardDiv);
         issueCount();
     }
 
 }
-loadIssues()
+
+
 
 
 // Button click 
@@ -110,29 +119,107 @@ function toggleStyle(id) {
 // open and close tabs 
 
 openBtn.addEventListener('click', function () {
-    console.log('open');
     allIssuesContainer.innerHTML = ''
-    loadingSpinner.classList.remove('hidden')
+    loadingSpinner.classList.remove('hidden');
     displayIssues(openData);
 
 })
 
 closeBtn.addEventListener('click', function () {
-    console.log('close');
-
     allIssuesContainer.innerHTML = ''
-    loadingSpinner.classList.remove('hidden')
+    loadingSpinner.classList.remove('hidden');
     displayIssues(closeData);
 
 })
-document.getElementById('all-btn').addEventListener('click', function () {
+
+allBtn.addEventListener('click', function () {
+    allIssuesContainer.innerHTML = ''
+    loadingSpinner.classList.remove('hidden');
     loadIssues();
+
 })
+
+// modal showing
+
+async function modalDetails(id) {
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    const data = await res.json()
+    displayModal(data.data);
+}
+
+const displayModal = (details) => {
+    const date = new Date(details.createdAt);
+    const formattedDate = date.toLocaleDateString();
+    const modalCard = document.getElementById("details-container");
+    modalCard.innerHTML = `
+     <h2 class="text-xl font-bold mb-3">${details.title}</h2>
+     <div class="flex gap-4 items-center">
+      <span class="badge badge-success rounded-2xl text-xl p-4">${details.status} </span>
+      <p class="text-gray-500" >Opened by</p>
+      <p class="text-gray-500 flex items-center gap-4"> <img src="./Ellipse 5.png" alt=""> ${details.assignee ? details.assignee : 'anonymous'}</p>
+      <p class="text-gray-500 flex items-center gap-4"><img src="./Ellipse 5.png" alt=""> ${formattedDate}</p>  
+     </div>
+
+     <div class=" mb-4">
+      <div class= "flex flex-wrap rounded-2xl">${showLabels(details.labels)}</div>
+     </div>
+
+    </div>
+    <div class="mb-4">
+      <p class="text-gray-500">${details.description}</p>
+    </div>
+
+    <div class="flex justify-between bg-blue-50 p-5 rounded-sm">
+      <div>
+         <p class="text-gray-500">Assignee</p>
+         <p class="font-bold">${details.author}</p>
+
+      </div>
+      <div>
+         <p class="text-gray-500 mb-3">Priority</p>
+         <span class="p-2 mt-2 rounded-xl font-bold ${details.priority == 'high'
+            ? 'text-red-500 font-bold bg-red-100'
+            : details.priority == 'medium'
+                ? 'text-yellow-500 bg-yellow-100 '
+                : 'text-gray-500 bg-gray-200 '
+
+        }">${details.priority}</span>
+      </div>
+      `
+    document.getElementById("modal_details").showModal()
+
+
+}
+
 
 loadIssues();
 
+document.getElementById('btn-search').addEventListener('click', (e) => {
+    e.preventDefault();
 
-// modal showing
+    const input = document.getElementById('input-search');
+    const searchValue = input.value.trim().toLowerCase();
+
+    // if search box is empty - show all value
+    const filteredIssues = searchValue
+        ? allIssues.filter(issue => issue.title.toLowerCase().includes(searchValue))
+        : allIssues;
+
+
+    displayIssues(filteredIssues);
+    allBtn.classList.remove('btn-primary');
+    openBtn.classList.remove('btn-primary');
+    closeBtn.classList.remove('btn-primary');
+
+    input.value = '';
+
+});
+
+
+
+
+
+
 
 
 
